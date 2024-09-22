48f9a6d5599fcb09a19b92784d2f9c4f4eaa5fe3
Justin <jgaston2606@gmail.com>
2024-09-21 23:44:31 -0400

    v2

--- src/Game.kt
+++ src/Game.kt
@@ -1,10 +1,57 @@
+import kotlin.random.Random
+
 fun main() {
-    println(wordList("ha", 3))
+    val randomNum = generateRandomNumber(1, 100)
+    playGame(randomNum)
 }
-fun wordList(word: String, count: Int): MutableList<String>{
-    val mutableList: MutableList<String> = mutableListOf()
-    for(i in 0..<count){
-        mutableList.add(word)
+
+fun generateRandomNumber(lower: Int, upper: Int): Int {
+    return Random.nextInt(lower, upper + 1)
+}
+
+fun playGame(randomNum: Int) {
+    val incorrectGuessList = mutableListOf<Int>()
+    var incorrectGuesses = 0
+    var guess: Int = getUserGuess()
+    while (guess != randomNum) {
+        guess = getUserGuess()
+        if (!incorrectGuessList.contains(guess) && guess != randomNum) {
+            incorrectGuesses++
+            incorrectGuessList.add(guess)
+        }
+        giveFeedback(guess, randomNum)
     }
-    return mutableList
+    endGame(randomNum, incorrectGuesses, incorrectGuessList)
+}
+fun getUserGuess(): Int {
+    var guess: Int
+    while (true) {
+        print("Enter a guess number between 1 and 100: ")
+        val input = readLine()
+        if (input != null && input.toIntOrNull() != null) {
+            guess = input.toInt()
+            if (guess in 1..100) {
+                return guess
+            } else {
+                println("Please enter a number between 1 and 100.")
+            }
+        } else {
+            println("Invalid input. Please enter a valid number.")
+        }
+    }
+}
+
+fun giveFeedback(guess: Int, randomNum: Int) {
+    when {
+        guess > randomNum && guess - 5 <= randomNum -> println("Close, but too big")
+        guess > randomNum -> println("Too big")
+        guess < randomNum && guess + 5 >= randomNum -> println("Close, but too small")
+        guess < randomNum -> println("Too small")
+    }
+}
+
+fun endGame(randomNum: Int, incorrectGuesses: Int, incorrectGuessList: List<Int>) {
+    println("Yay, you won! The number was $randomNum")
+    println("Number of incorrect guesses: $incorrectGuesses")
+    println("Incorrect guesses: $incorrectGuessList")
 }
